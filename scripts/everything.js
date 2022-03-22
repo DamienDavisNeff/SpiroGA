cd61ebb3c7b6fd7895a0c5b212224d81322f99e0
Thirdplace Studios <thirdplace.studios.dev@gmail.com>
2022-03-22 13:55:00 -0400

    Uploaded Scripts

---
+++ scripts/everything.js
@@ -0,0 +1,19 @@
+function youtubeClick() {
+    window.open("https://www.youtube.com/goldenspyroofficial?sub_confirmation=1");
+}
+
+function twitchClick() {
+    window.open("https://www.twitch.tv/sirspiroszn");
+}
+
+function twitterClick() {
+    window.open("https://www.twitter.com/sirspiroszn");
+}
+
+function instagramClick() {
+    window.open("https://www.twitter.com/sirspiro");
+}
+
+function discordClick() {
+    window.open("https://www.discord.com/invite/xz3S4nk");
+}
---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+function learnMoreClick() {
+    window.open("http://spiro.ga/everything");
+}
